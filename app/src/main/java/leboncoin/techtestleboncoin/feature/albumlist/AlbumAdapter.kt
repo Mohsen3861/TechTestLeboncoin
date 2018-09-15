80faaf19cd1c53f2046f2b8140404c94c1e044e1
Mohsen3861 <mohsenraic@hotmail.fr>
2018-09-16 00:34:03 +0200

    create albums recyclerview and its adapter

--- app/src/main/java/leboncoin/techtestleboncoin/feature/albumlist/AlbumAdapter.kt
+++ app/src/main/java/leboncoin/techtestleboncoin/feature/albumlist/AlbumAdapter.kt
@@ -1,4 +1,41 @@
 package leboncoin.techtestleboncoin.feature.albumlist
 
-class AlbumAdapter {
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+import leboncoin.techtestleboncoin.R
+
+class AlbumAdapter(val albumList: List<Album>): RecyclerView.Adapter<AlbumAdapter.ViewHolder>() {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.albumTitle?.text = albumList[position].title
+
+        Picasso.get()
+                .load(albumList[position].thumbnailUrl)
+                .resize(50, 50)
+                .centerCrop()
+                .into(holder.albumImage)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.albums_list_item, parent, false)
+        return ViewHolder(v);
+
+    }
+
+    override fun getItemCount(): Int {
+        return albumList.size
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val albumTitle = itemView.findViewById<TextView>(R.id.titleTextView)
+        val albumImage = itemView.findViewById<ImageView>(R.id.albumImageView)
+
+    }
+
 }
--- app/src/main/java/leboncoin/techtestleboncoin/feature/albumlist/AlbumsActivity.kt
+++ app/src/main/java/leboncoin/techtestleboncoin/feature/albumlist/AlbumsActivity.kt
@@ -2,9 +2,13 @@ package leboncoin.techtestleboncoin.feature.albumlist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.widget.LinearLayout
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_albums.*
 import leboncoin.techtestleboncoin.R
 import leboncoin.techtestleboncoin.feature.albumlist.repository.AlbumRepositoryProvider
 
@@ -14,6 +18,8 @@ class AlbumsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_albums)
 
+
+        albumsRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
 
         retrieveAllAlbums()
     }
@@ -30,6 +36,8 @@ class AlbumsActivity : AppCompatActivity() {
                .subscribe ({
                    result ->
                    Log.d("Result", "There are ${result.size} albums")
+                   albumsRecyclerView.adapter = AlbumAdapter(result)
+
                }, { error ->
                    error.printStackTrace()
                })
